5570d4fdec753fce5bf05d67447e772735ce7d48
Gabriel Mariani <gmariani405@gmail.com>
2018-08-08 18:13:16 -0400

    Update URLs to HTTPS

--- app/service-worker.js
+++ app/service-worker.js
@@ -25,7 +25,7 @@ const config = {
     cachePathPattern: /\/(?:(css|img|js)\/(.+)?)?$/,
     offlineImage:
         '<svg role="img" aria-labelledby="offline-title"' +
-        ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">' +
+        ' viewBox="0 0 400 300" xmlns="https://www.w3.org/2000/svg">' +
         '<title id="offline-title">Offline</title>' +
         '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>' +
         '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">' +
